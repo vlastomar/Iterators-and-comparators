1b1bb9b1459a380fa8f62a83f6adcaf8879a60c3
VSM <vlad24@abv.bg>
2020-09-09 08:00:12 +0300

    book

---
+++ BookPack/Main.java
@@ -0,0 +1,21 @@
+package BookPack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+        Book bookThree = new Book("The Documents in the Case", 2002);
+        Book bookTwo = new Book("The Documents in the Case", 1930, "Dorothy Sayers", "Robert Eustace");
+
+        List<Book> books = new ArrayList<>();
+        books.add(bookOne);
+        books.add(bookTwo);
+        books.add(bookThree);
+
+    }
+}
